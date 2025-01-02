ac49138808e4a3dcedadac20c342ec2c4c070514
Wilson Klaen <wilson4059@gmail.com>
2025-01-02 17:14:05 -0300

    Conexao com o banco de dados

--- LeiloesTDSat/LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/conectaDAO.java
@@ -11,7 +11,7 @@ public class conectaDAO {
 
         try {
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=Wil40554645wil");
 
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
